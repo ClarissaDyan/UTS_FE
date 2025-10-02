e0ac37cef1a69366281e23f0e002545c57d69ff0
Zefery <yusufaslam360@gmail.com>
2025-10-02 14:17:22 +0700

    push payment, fix

--- js/authGuard.js
+++ js/authGuard.js
@@ -1,16 +1,11 @@
-// Mengimpor fungsi untuk mendapatkan data user dari localStorage
 import { getCurrentUser } from './localStorageManager.js';
 
-// Fungsi ini akan langsung berjalan saat script dimuat
 (function() {
     const currentUser = getCurrentUser();
 
-    // Periksa apakah tidak ada pengguna yang login
     if (!currentUser) {
-        // Jika tidak ada, tampilkan pesan peringatan
         alert('Anda harus login terlebih dahulu untuk mengakses halaman ini.');
         
-        // Alihkan pengguna ke halaman login
         window.location.href = 'loginSignUp.html';
     }
 })();
